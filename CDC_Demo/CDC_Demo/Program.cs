73167b87b4cf8999f381c803144dcfedad3942b5
Suyog Gadal <suyog.bishwokarma@cginfinity.com>
2022-10-17 12:01:35 +0530

    First commit

---
+++ CDC_Demo/CDC_Demo/Program.cs
@@ -0,0 +1,142 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http.Headers;
+
+
+
+CDC_Demo CDC_Demo = new CDC_Demo();
+
+CDC_Demo.GetDataFromAPI();
+Environment.Exit(0);
+public class CDC_Demo
+{
+    public static void GetDataFromAPI()
+    {
+        try
+        {
+            Console.WriteLine("Fetching data from API Start");
+            string json = new WebClient().DownloadString("http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json");
+
+            Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json);
+
+            Console.WriteLine("Fetching data from API End");
+
+            Console.WriteLine("Saving API data in Azure SQL Start");
+
+            BulkDataInsertIntoSQL(myDeserializedClass);
+
+            Console.WriteLine("Saving API data in Azure SQL End");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error :- " + ex.Message);
+            Console.ReadLine();
+        }
+    }
+
+    public static void BulkDataInsertIntoSQL(Root myDeserializedClass)
+    {
+        try
+        {
+            DataTable tbl = new DataTable();
+            tbl.Columns.Add(new DataColumn("timepoint", typeof(Int32)));
+            tbl.Columns.Add(new DataColumn("cloudcover", typeof(Int32)));
+            tbl.Columns.Add(new DataColumn("seeing", typeof(Int32)));
+            tbl.Columns.Add(new DataColumn("transparency", typeof(Int32)));
+            tbl.Columns.Add(new DataColumn("lifted_index", typeof(Int32)));
+            tbl.Columns.Add(new DataColumn("rh2m", typeof(Int32)));
+            tbl.Columns.Add(new DataColumn("wind10mdirection", typeof(string)));
+            tbl.Columns.Add(new DataColumn("wind10mspeed", typeof(Int32)));
+            tbl.Columns.Add(new DataColumn("temp2m", typeof(Int32)));
+            tbl.Columns.Add(new DataColumn("prec_type", typeof(string)));
+
+
+            for (int i = 0; i < myDeserializedClass.dataseries.Count(); i++)
+            {
+                DataRow dr = tbl.NewRow();
+                dr["timepoint"] = myDeserializedClass.dataseries[i].timepoint;
+                dr["cloudcover"] = myDeserializedClass.dataseries[i].cloudcover;
+                dr["seeing"] = myDeserializedClass.dataseries[i].seeing;
+                dr["transparency"] = myDeserializedClass.dataseries[i].transparency;
+                dr["lifted_index"] = myDeserializedClass.dataseries[i].lifted_index;
+                dr["rh2m"] = myDeserializedClass.dataseries[i].rh2m;
+                dr["wind10mdirection"] = myDeserializedClass.dataseries[i].wind10m.direction;
+                dr["wind10mspeed"] = myDeserializedClass.dataseries[i].wind10m.speed;
+                dr["temp2m"] = myDeserializedClass.dataseries[i].temp2m;
+                dr["prec_type"] = myDeserializedClass.dataseries[i].prec_type;
+
+                tbl.Rows.Add(dr);
+            }
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "40.124.98.11"; //"tcp:sqlserver-cbiedetest.database.windows.net";
+            builder.UserID = "CGIAdmin";
+            builder.Password = "CGITesting1@";
+            builder.InitialCatalog = "CDCSourceDB";
+
+            //string connection = "Data Source=40.124.138.4;Initial Catalog=StreamingTest;User Id = cgadmin; Password = CGInfinity1@";
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                //create object of SqlBulkCopy which help to insert  
+                SqlBulkCopy objbulk = new SqlBulkCopy(connection);
+
+                //assign Destination table name  
+                objbulk.DestinationTableName = "data";
+
+                objbulk.ColumnMappings.Add("timepoint", "timepoint");
+                objbulk.ColumnMappings.Add("cloudcover", "cloudcover");
+                objbulk.ColumnMappings.Add("seeing", "seeing");
+                objbulk.ColumnMappings.Add("transparency", "transparency");
+                objbulk.ColumnMappings.Add("lifted_index", "lifted_index");
+                objbulk.ColumnMappings.Add("rh2m", "rh2m");
+                objbulk.ColumnMappings.Add("wind10mdirection", "wind10mdirection");
+                objbulk.ColumnMappings.Add("wind10mspeed", "wind10mspeed");
+                objbulk.ColumnMappings.Add("temp2m", "temp2m");
+                objbulk.ColumnMappings.Add("prec_type", "prec_type");
+
+                connection.Open();
+                //insert bulk Records into DataBase.  
+                objbulk.WriteToServer(tbl);
+                connection.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error :- " + ex.Message);
+            Console.ReadLine();
+        }
+    }
+}
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+public class Dataseries
+{
+    public int timepoint { get; set; }
+    public int cloudcover { get; set; }
+    public int seeing { get; set; }
+    public int transparency { get; set; }
+    public int lifted_index { get; set; }
+    public int rh2m { get; set; }
+    public Wind10m wind10m { get; set; }
+    public int temp2m { get; set; }
+    public string prec_type { get; set; }
+}
+
+public class Root
+{
+    public string product { get; set; }
+    public string init { get; set; }
+    public List<Dataseries> dataseries { get; set; }
+}
+
+public class Wind10m
+{
+    public string direction { get; set; }
+    public int speed { get; set; }
+}
+
---
+++ SQL2AEH/SQL2AEH/Program.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SQL2AEH
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> list, int partsSize)
+        {
+            return list.Select((item, index) => new { index, item })
+                       .GroupBy(x => x.index / partsSize)
+                       .Select(x => x.Select(y => y.item));
+        }
+    }
+
+    public class SqlTextQuery
+    {
+        private readonly string _sqlDatabaseConnectionString;
+
+        public SqlTextQuery(string sqlDatabaseConnectionString)
+        {
+            _sqlDatabaseConnectionString = sqlDatabaseConnectionString;
+        }
+
+        public IEnumerable<Dictionary<string, object>> PerformQuery(string query)
+        {
+            var command = new SqlCommand(query);
+            //CommandType.Text;
+
+            IEnumerable<Dictionary<string, object>> result = null;
+            using (var sqlConnection = new SqlConnection(_sqlDatabaseConnectionString))
+            {
+                sqlConnection.Open();
+
+                command.Connection = sqlConnection;
+                using (SqlDataReader r = command.ExecuteReader())
+                {
+                    result = Serialize(r);
+                }
+                sqlConnection.Close();
+            }
+            return result;
+        }
+
+        private IEnumerable<Dictionary<string, object>> Serialize(SqlDataReader reader)
+        {
+            var results = new List<Dictionary<string, object>>();
+            while (reader.Read())
+            {
+                var row = new Dictionary<string, object>();
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row.Add(reader.GetName(i), reader.GetValue(i));
+                }
+                results.Add(row);
+            }
+            return results;
+        }
+    }
+
+    class Program
+    {
+
+        static void Main()
+        {
+            try
+            {
+                // CONFIGURABLE VARIABLES
+                int SQLBatchSize = Convert.ToInt32(ConfigurationManager.AppSettings["SQLBatchSize"]);
+                int ExecutionControl = Convert.ToInt32(ConfigurationManager.AppSettings["ExecutionControl"]);
+                int ExecutionControlSleepMs = Convert.ToInt32(ConfigurationManager.AppSettings["ExecutionControlSleepMs"]);
+
+                // VARIABLES
+                string selectDataQuery;
+
+                // GET SQL & HUB CONNECTION STRINGS
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "40.124.98.11"; //"tcp:sqlserver-cbiedetest.database.windows.net";
+                builder.UserID = "CGIAdmin";
+                builder.Password = "CGITesting1@";
+                builder.InitialCatalog = "CDCSourceDB";
+               
+                //string sqlDatabaseConnectionString = ConfigurationManager.AppSettings["sqlDatabaseConnectionString"];
+                Console.WriteLine(" SQL Connection String : - " + builder.ConnectionString);
+                string sqlDatabaseConnectionString = builder.ConnectionString;
+                string serviceBusConnectionString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ServiceBusConnectionString"];
+                string hubName = ConfigurationManager.AppSettings["Microsoft.ServiceBus.EventHubToUse"];
+                Console.WriteLine("Event hub connection string" + serviceBusConnectionString);
+                Console.WriteLine("Event hub name" + hubName);
+                // GET SQL SERVER SOURCE TABLE
+                string dataTableName = ConfigurationManager.AppSettings["DataTableName"];
+                string dataTableName_CT = "cdc." + dataTableName.Replace(".", "_") + "_CT";
+
+                // ESTABLISH SQL & HUB CONNECTIONS
+                SqlTextQuery queryPerformer = new SqlTextQuery(sqlDatabaseConnectionString);
+                EventHubClient eventHubClient = EventHubClient.CreateFromConnectionString(serviceBusConnectionString, hubName);
+
+                do
+                {
+                    Console.WriteLine("Data fetching from CDC SQL table start");
+
+                    selectDataQuery = "GetCDCTableData";
+                    IEnumerable<Dictionary<string, object>> resultCollection = queryPerformer.PerformQuery(selectDataQuery);
+
+                    Console.WriteLine("Data fetching from CDC SQL table end");
+                    
+                    if (resultCollection.Any())
+                    {                        
+                        Console.WriteLine("Data sending to Event Hub in 5 batchsize");
+                        foreach (var resultGroup in resultCollection.Split(SQLBatchSize))
+                        {                            
+                            SendRowsToEventHub(eventHubClient, resultGroup).Wait();                            
+                        }
+                        Console.WriteLine("Data sending to Event Hub completed");
+                    }
+                    
+                    Thread.Sleep(ExecutionControlSleepMs);
+                }
+                while (ExecutionControl == 1); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error :- " + ex.Message);
+            }
+        }
+
+        private static async Task SendRowsToEventHub(EventHubClient eventHubClient, IEnumerable<object> rows)
+        {
+            var memoryStream = new MemoryStream();
+
+            using (var sw = new StreamWriter(memoryStream, new UTF8Encoding(false), 1024, leaveOpen: true))
+            {
+                string serialized = JsonConvert.SerializeObject(rows);
+                sw.Write(serialized);
+                sw.Flush();
+            }
+
+            Debug.Assert(memoryStream.Position > 0, "memoryStream.Position > 0");
+
+            memoryStream.Position = 0;
+            EventData eventData = new EventData(memoryStream);
+
+            await eventHubClient.SendAsync(eventData);
+        }
+    }
+}
